ccd7d526df6ab67201b80ac0ab8e6d32fd4bef70
hamidfawzi97 <35073262+hamidfawzi97@users.noreply.github.com>
2019-04-27 04:21:36 +0200

    Update Deep Learning.py

--- Deep Learning.py
+++ Deep Learning.py
@@ -1,19 +1,23 @@
-
 import json
 import os
 import tensorflow as tf
+import numpy as np
 
 from data.dataset import get_training_data, get_test_data
 from models.train_model import TrainModel
 from data.utils import show_sample
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 
-from sklearn.metrics import (precision_score, recall_score, f1_score)
+import warnings
+warnings.filterwarnings('ignore')
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-tf.app.flags.DEFINE_string('train_path', os.path.abspath(os.path.join(os.path.dirname( "__file__" ), '..', 'data/tf_records/training_file_0.tfrecord')), 
+tf.app.flags.DEFINE_string('train_path', os.path.abspath(os.path.join(os.path.dirname( "__file__" ), '..', 'data/tf_records/trainingg_file_0.tfrecord')),
                            'Path for the training data.')
-tf.app.flags.DEFINE_string('test_path', os.path.abspath(os.path.join(os.path.dirname("__file__"), '..', 'data/tf_records/test_file_0.tfrecord')), 
+tf.app.flags.DEFINE_string('test_path', os.path.abspath(os.path.join(os.path.dirname("__file__"), '..', 'data/tf_records/testt_file_0.tfrecord')),
                            'Path for the test data.')
 
 tf.app.flags.DEFINE_string('word2idx', os.path.abspath(os.path.join(os.path.dirname("__file__"), '..', 'data/preprocessed/word2idx.txt')), 
@@ -22,17 +26,17 @@ tf.app.flags.DEFINE_string('word2idx', os.path.abspath(os.path.join(os.path.dirn
 tf.app.flags.DEFINE_string('checkpoints_path', os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'checkpoints/model.ckpt')), 
                            'Path for the test data.')
 
-tf.app.flags.DEFINE_integer('num_epoch', 1000,
+tf.app.flags.DEFINE_integer('num_epoch', 10,
                             'Number of training epoch.'
                             )
 tf.app.flags.DEFINE_integer('batch_size', 32,
                             'Batch size of the training set.'
                             )
-tf.app.flags.DEFINE_float('learning_rate', 0.0005,
+tf.app.flags.DEFINE_float('learning_rate', 0.0015,
                           'Learning rate of optimizer.'
                           )
 
-tf.app.flags.DEFINE_string('architecture', 'unidirectional',
+tf.app.flags.DEFINE_string('architecture', 'bidirectional',
                           'Type of LSTM-Architecture, choose between "unidirectional" or "bidirectional"'
                           )
 
@@ -50,11 +54,11 @@ tf.app.flags.DEFINE_integer('num_classes', 2,
                             'Number of output classes.'
                             )
 
-tf.app.flags.DEFINE_integer('n_train_samples', 8529,
+tf.app.flags.DEFINE_integer('n_train_samples', 1000,
                             'Number of all training sentences.'
                             )
 
-tf.app.flags.DEFINE_integer('n_test_samples', 2133,
+tf.app.flags.DEFINE_integer('n_test_samples', 1000,
                             'Number of all training sentences.'
                             )
 tf.app.flags.DEFINE_float('required_acc_checkpoint', 0.7,
@@ -67,7 +71,8 @@ FLAGS = tf.app.flags.FLAGS
 
 
 def main(_):
-    
+
+
     with open(FLAGS.word2idx) as json_file:  
         word2idx = json.load(json_file)
 
@@ -77,16 +82,16 @@ def main(_):
         
         train_model=TrainModel(FLAGS, len(word2idx))
         
-        training_dataset=get_training_data(FLAGS)
-        test_dataset=get_test_data(FLAGS)
+        training_dataset = get_training_data(FLAGS)
+        test_dataset = get_test_data(FLAGS)
         
         iterator_train = training_dataset.make_initializable_iterator()
-        iterator_test=test_dataset.make_initializable_iterator()
+        iterator_test = test_dataset.make_initializable_iterator()
         
         x_train, y_train, _, seq_length_train = iterator_train.get_next()
-        
         x_test, y_test, _, seq_length_test =iterator_test.get_next()
-    
+
+
         dropout_keep_prob = tf.placeholder(tf.float32, name='dropout_keep_prob')
         
         logits, probs=train_model.compute_prediction(x_train, seq_length_train, dropout_keep_prob, reuse_scope=False)
@@ -98,12 +103,15 @@ def main(_):
         logits_test, probs_test=train_model.compute_prediction(x_test, seq_length_test, dropout_keep_prob, reuse_scope=True) 
         accuracy_test = train_model.compute_accuracy(probs_test, y_test)
 
+        # print("prec",y_test)
         saver=tf.train.Saver()
-        
+
     with tf.Session(graph=training_graph) as sess:
-        
+
+        # print(sess.run(type))
         sess.run(tf.global_variables_initializer())
-        
+
+
         n_batches=int(FLAGS.n_train_samples/FLAGS.batch_size)   
 
         for epoch in range(FLAGS.num_epoch):
@@ -113,41 +121,60 @@ def main(_):
             
             traininig_loss=0
             training_acc=0
-            
-              
+
+
+
             feed_dict={dropout_keep_prob:0.5}
-            
-           
+
+            prb_test = sess.run(probs_test, feed_dict)
+            y_ttest = sess.run(y_test, feed_dict)
+
+            prb_test2 = []
+
+            y_ttest2 = []
+
+            for x in prb_test:
+                if x[0] >= 0.5:
+                    prb_test2.append(1)
+                else:
+                    prb_test2.append(0)
+
+
+            for y in y_ttest:
+               y_ttest2.append(y[0])
+
+
+
+            # print(type(y_ttest))
+        
+
             for n_batch in range(0, n_batches):
               
                 _, l, acc, logits_, probs_=sess.run((train_op, loss, accuracy_train, logits, probs), feed_dict)
-                                
+
                 traininig_loss+=l
                 training_acc+=acc
                 
-                
+
                   
             feed_dict={dropout_keep_prob:1.0}
                 
             acc_avg_test=sess.run(accuracy_test, feed_dict)
-       
-            loss_avg=traininig_loss/n_batches
             acc_avg_train=training_acc/n_batches
 
-            print('epoch_nr: %i, train_loss: %.3f, train_acc: %.3f, test_acc: %.3f'%(epoch, loss_avg, acc_avg_train, acc_avg_test))
 
-            traininig_loss=0
-            training_acc=0
-            
-            show_sample(FLAGS, sess, logits_test, probs_test, dropout_keep_prob, x)
-            
-            if self.FLAGS.required_acc_checkpoint>0.70:
-                saver.save(sess, FLAGS.checkpoints_path)
-    print("\tPrecision: %1.3f" % precision_score(y_train, probs))
-    print("\tRecall: %1.3f" % recall_score(y_train, probs))
-    print("\tF1: %1.3f\n" % f1_score(y_train, probs))
+
+
+            print('Epoch: %i, Accuracy: %.3f'%(epoch, acc_avg_test))
+
+            print("\tPrecision: %1.3f" % precision_score(y_ttest2, prb_test2))
+            print("\tRecall: %1.3f" % recall_score(y_ttest2, prb_test2))
+            print("\tF1: %1.3f\n" % f1_score(y_ttest2, prb_test2))
+
+
+
 
 if __name__ == "__main__":
     
     tf.app.run() 
-    
+
